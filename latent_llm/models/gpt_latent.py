be42fcaa7b64a7c925ceda2e3e075004c84c3875
toilaluan <toilaluan@github.com>
2025-03-14 20:38:24 +0700

    update

--- latent_llm/models/gpt_latent.py
+++ latent_llm/models/gpt_latent.py
@@ -128,18 +128,6 @@ class LatentEncoder(nn.Module):
         self, input_ids: torch.Tensor, pad_token_id: int
     ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
         B = input_ids.size(0)
-        if input_ids.size(1) < self.block_size:
-            input_ids = torch.cat(
-                [
-                    input_ids,
-                    torch.full(
-                        (B, self.block_size - input_ids.size(1)),
-                        pad_token_id,
-                        device=input_ids.device,
-                    ).long(),
-                ],
-                dim=1,
-            )
         embeds = self.model.get_input_embeddings()(input_ids)
         position_ids = self.position_ids.repeat(B, 1)
         masks = input_ids != pad_token_id
