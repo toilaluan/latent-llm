efddacb3e4440bf62574f65911f0e66ed6397e6f
toilaluan <toilaluan@github.com>
2025-03-17 00:42:25 +0700

    a

--- latent_llm/get_tokenizer.py
+++ latent_llm/get_tokenizer.py
@@ -3,4 +3,5 @@ from transformers import AutoTokenizer
 
 def get_tokenizer(model_name: str):
     tokenizer = AutoTokenizer.from_pretrained(model_name)
+    tokenizer.padding_side = "right"
     return tokenizer
