54ce9307606f35c35f018163b8dc4f7ef32ff636
toilaluan <toilaluan@github.com>
2025-03-16 23:40:43 +0700

    a

--- latent_llm/get_tokenizer.py
+++ latent_llm/get_tokenizer.py
@@ -3,5 +3,4 @@ from transformers import AutoTokenizer
 
 def get_tokenizer(model_name: str):
     tokenizer = AutoTokenizer.from_pretrained(model_name)
-    tokenizer.add_special_tokens({"pad_token": "<|pad|>"})
     return tokenizer
