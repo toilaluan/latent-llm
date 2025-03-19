a18ec77dd088f16796ca9262db05b3d0a4b37bb4
toilaluan <toilaluan@github.com>
2025-03-19 15:40:48 +0700

    a

--- latent_llm/train_flow_matching.py
+++ latent_llm/train_flow_matching.py
@@ -297,6 +297,7 @@ def train_step(
 
     # Sample random timesteps
     batch_size = suffix_tokens.size(0)
+    print(suffix_latents.shape)
     optimizer.zero_grad()
 
     timesteps = torch.randint(
